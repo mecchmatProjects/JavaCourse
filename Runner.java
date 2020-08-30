c06fa9a367a7f4ef497df4071d4a33e7fa419b24
mecchmatProjects <33247296+mecchmatProjects@users.noreply.github.com>
2020-08-30 19:13:17 +0300

    add 8th lecture

---
+++ Runner.java
@@ -0,0 +1,11 @@
+public class Runner {
+public static void main(String[] args) {
+// Mark<String> ms = new Mark<String>(“7”); //ошибка компиляции
+Mark<Double> md = new Mark<Double>(71.4D);//71.5d
+System.out.println(md.sameAny(md));
+Mark<Integer> mi = new Mark<Integer>(71);
+System.out.println(md.sameAny(mi));
+// md.same(mi); //ошибка компиляции
+System.out.println(md.roundMark());
+}
+}
