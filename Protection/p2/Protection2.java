c44c185e7ff765a39996a8ad7c6b2b60c497ce81
mecchmatProjects <33247296+mecchmatProjects@users.noreply.github.com>
2020-08-30 19:04:00 +0300

    add examples 5th

---
+++ Protection/p2/Protection2.java
@@ -0,0 +1,39 @@
+package p2;
+
+
+class Protection2 extends p1.Protection {
+
+  Protection2() {
+    System.out.println("derived other package constructor");
+
+//  class or package only
+//  System.out.println("n = " + n);
+//  class only
+//  System.out.println("n_pri = " + n_pri);
+
+    System.out.println("n_pro = " + n_pro);
+    System.out.println("n_pub = " + n_pub);
+  }
+}
+
+
+
+class OtherPackage {
+
+  OtherPackage() {
+
+    p1.Protection p = new p1.Protection();
+    System.out.println("other package constructor");
+
+//  class or package only
+//  System.out.println("n = " + p.n);
+//  class only
+//  System.out.println("n_pri = " + p.n_pri);
+//  class, subclass or package only
+//  System.out.println("n_pro = " + p.n_pro);
+
+    System.out.println("n_pub = " + p.n_pub);
+
+  }
+
+}
